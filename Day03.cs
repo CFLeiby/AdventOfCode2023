fb3756ca7908f7fd09ee14a3aee036e48397825e
cleiby <cleiby@signifyhealth.com>
2023-12-05 11:27:01 -0500

    Day 3, Part 2

--- Day03.cs
+++ Day03.cs
@@ -3,8 +3,11 @@ namespace AdventOfCode;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-public class Day03 : IDayChallenge 
+public class Day03 : IDayChallenge
 {
+    private string[] _lines;
+    private IEnumerable<Tuple<int, int>>[] _partNosByLine;
+    
     public Task Execute()
     {
         using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"AdventOfCode.Inputs.Day03.txt"))
@@ -17,32 +20,111 @@ public class Day03 : IDayChallenge
 
             using (var reader = new StreamReader(stream))
             {
-                var lines = reader.ReadToEnd().Split(Environment.NewLine);
-                var partNumberTotal = 0;
-
-                //part numbers from the first line
-                partNumberTotal += GetPartNumbers(lines[0], null, lines[1]).Sum();
+                _lines = reader.ReadToEnd().Split(Environment.NewLine);
+                _partNosByLine = new IEnumerable<Tuple<int, int>>[_lines.Length];
                 
-                //part numbers from the last line
-                var lastLine = lines.Length - 1;
-                partNumberTotal += GetPartNumbers(lines[lastLine],lines[lastLine -1], null).Sum();
+                //First line
+                _partNosByLine[0] = GetPartNumbers(_lines[0], null, _lines[1]);
+                
+                //Last line
+                var lastLine = _lines.Length - 1;
+                _partNosByLine[lastLine] = GetPartNumbers(_lines[lastLine],_lines[lastLine -1], null);
 
-                //part numbers for each line in between
+                //Each line in between
                 for(var i = 1; i < lastLine; i++)
                 {
-                    partNumberTotal += GetPartNumbers(lines[i], lines[i-1], lines[i+1]).Sum();
+                    _partNosByLine[i] = GetPartNumbers(_lines[i], _lines[i-1], _lines[i+1]);
                 }
-                
-                Console.WriteLine($"Sum of all part numbers: {partNumberTotal}");
+
+                var partNoTotal = 0;
+                var gearRatioTotal = 0;
+                for (var i = 0; i < _lines.Length; i++)
+                {
+                    partNoTotal += _partNosByLine[i].Sum(pn =>
+                    {
+                        var partNo = _lines[i].Substring(pn.Item1, pn.Item2 - pn.Item1 + 1);
+                        return int.Parse(partNo);
+                    });
+
+                    gearRatioTotal += GetGearRatios(i).Sum();
+                }
+
+                Console.WriteLine($"Sum of all part numbers: {partNoTotal}");
+                Console.WriteLine($"Sum of all gear ratios: {gearRatioTotal}");
             }
         }
 
         return Task.CompletedTask;
     }
 
-    private static IEnumerable<int> GetPartNumbers(string line, string lineAbove, string lineBelow)
+    private IEnumerable<int> GetGearRatios(int lineNumber)
     {
-        var partNumbers = new List<int>();
+        //Find all the gear symbols in the line
+        var potentialGears = Regex.Matches(_lines[lineNumber], @"\*");
+        var gearRatios = new List<int>();
+        foreach (Match potentialGear in potentialGears)
+        {
+            var touchingPartNos = new List<int>();
+
+            //is there a part number to the left?
+            var range = new Tuple<int, int>(potentialGear.Index - 1, potentialGear.Index + 1);
+            var part = _partNosByLine[lineNumber].FirstOrDefault(pn => pn.Item2 == range.Item1);
+            if (part != null)
+            {
+                var partNo = int.Parse(_lines[lineNumber].Substring(part.Item1, part.Item2 - part.Item1 + 1));
+                touchingPartNos.Add(partNo);
+            }
+
+            //is there a part number to the right?
+            part = _partNosByLine[lineNumber].FirstOrDefault(pn => pn.Item1 == range.Item2);
+            if (part != null)
+            {
+                var partNo = int.Parse(_lines[lineNumber].Substring(part.Item1, part.Item2 - part.Item1 + 1));
+                touchingPartNos.Add(partNo);
+            }
+
+            //How many are above?
+            if (lineNumber > 0)
+            {
+                foreach (var partNo in _partNosByLine[lineNumber - 1].Where(pn =>
+                             (pn.Item1 >= range.Item1 && pn.Item1 <= range.Item2) ||
+                             (pn.Item2 >= range.Item1 && pn.Item2 <= range.Item2)))
+                {
+                    var number = int.Parse(_lines[lineNumber - 1].Substring(partNo.Item1, partNo.Item2 - partNo.Item1 + 1));
+                    touchingPartNos.Add(number);                
+                }
+            }
+            
+            //got too many yet?
+            if (touchingPartNos.Count > 2)
+            {
+                continue;
+            }
+            
+            //How many below?
+            if (lineNumber < _lines.Length - 1)
+            {
+                foreach (var partNo in _partNosByLine[lineNumber + 1].Where(pn =>
+                             (pn.Item1 >= range.Item1 && pn.Item1 <= range.Item2) ||
+                             (pn.Item2 >= range.Item1 && pn.Item2 <= range.Item2)))
+                {
+                    var number = int.Parse(_lines[lineNumber + 1].Substring(partNo.Item1, partNo.Item2 - partNo.Item1 + 1));
+                    touchingPartNos.Add(number);                
+                }
+            }
+
+            if (touchingPartNos.Count == 2)
+            {
+                gearRatios.Add(touchingPartNos[0] * touchingPartNos[1]);
+            }
+        }
+
+        return gearRatios;
+    }
+    
+    private static IEnumerable<Tuple<int, int>> GetPartNumbers(string line, string lineAbove, string lineBelow)
+    {
+        var partNumbers = new List<Tuple<int, int>>();
 
         var potentialNumbers = Regex.Matches(line, @"\d+");
         foreach (Match potentialNumber in potentialNumbers)
@@ -51,7 +133,7 @@ public class Day03 : IDayChallenge
                 new Tuple<int, int>(potentialNumber.Index, potentialNumber.Index + potentialNumber.Length-1);
             if (IsPartNumber(numberRange, line, lineAbove, lineBelow))
             {
-                partNumbers.Add(int.Parse(potentialNumber.Value));
+                partNumbers.Add(numberRange);
             }
         }
         return partNumbers;
