a2eeeddc887faac75f1bea3d8ce7cac22405e561
cleiby <cleiby@signifyhealth.com>
2023-12-06 12:29:56 -0500

    Wildly inefficient Part 2 for Day 5

--- Day05.cs
+++ Day05.cs
@@ -14,53 +14,85 @@ public class Day05 : IDayChallenge
     private List<MapRange> TempToHumidity = new();
     private List<MapRange> HumidityToLoc = new();
 
-    private record MapRange(long SourceRangeStart, long SourceRangeEnd, long DestinationRangeStart);
+    private record MapRange(long SourceRangeStart, long SourceRangeEnd, long DestinationRangeStart)
+    {
+        public long MapValue(long sourceValue)
+        {
+            return DestinationRangeStart + sourceValue - SourceRangeStart;
+        }
+    }
 
-    public Task Execute()
+    public async Task Execute()
     {
         try
         {
             LoadMaps();
-            IEnumerable<long> seeds;
+            List<long> seeds;
             
             using (var stream = Assembly.GetExecutingAssembly()
                        .GetManifestResourceStream($"AdventOfCode.Inputs.Day05.Seeds.txt"))
             {
                 if (stream == null)
                 {
-                    throw new FileNotFoundException($"Seeds.txt not found");
+                    Console.WriteLine($"Seeds.txt not found");
+                    return;
                 }
 
                 using (var reader = new StreamReader(stream))
                 {
-                    var line = reader.ReadLine();
-                    seeds = line?.Split(' ').Select(long.Parse);
+                    var line = await reader.ReadLineAsync();
+                    seeds = line?.Split(' ').Select(long.Parse).ToList();
                 }
             }
 
             if (seeds == null)
             {
                 Console.WriteLine("Where are the seeds?");
-                return Task.CompletedTask;
+                return;
             }
             
             var closestLocation = (from seed in seeds 
                 select MapValue(seed, SeedToSoil) into soil 
-                select MapValue(soil, SoilToFertilizer) into fert 
-                select MapValue(fert, FertilizerToWater) into water 
+                select MapValue(soil, SoilToFertilizer) into fertilize 
+                select MapValue(fertilize, FertilizerToWater) into water 
                 select MapValue(water, WaterToLight) into light 
                 select MapValue(light, LightToTemp) into temp 
                 select MapValue(temp, TempToHumidity) into humidity 
                 select MapValue(humidity, HumidityToLoc)).Min();
-            
-            Console.WriteLine($"Closest location: {closestLocation}");
+            Console.WriteLine($"Closest location (discrete numbers): {closestLocation}");
+
+            var tasks = new List<Task<long>>();
+            for (var i = 0; i < seeds.Count; i += 2)
+            {
+                var rangeStart = seeds[i];
+                var rangeSize = seeds[i + 1];
+                tasks.Add(Task.Run(() =>
+                {
+                    closestLocation = long.MaxValue;
+                    for (var j = 0; j < rangeSize; j++)
+                    {
+                        var seed =  rangeStart + j;
+                        var soil = MapValue(seed, SeedToSoil);
+                        var fertilize = MapValue(soil, SoilToFertilizer);
+                        var water = MapValue(fertilize, FertilizerToWater);
+                        var light = MapValue(water, WaterToLight);
+                        var temp = MapValue(light, LightToTemp);
+                        var humidity = MapValue(temp, TempToHumidity);
+                        var loc = MapValue(humidity, HumidityToLoc);
+                        closestLocation = Math.Min(closestLocation, loc);
+                    }
+
+                    return closestLocation;
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+            Console.WriteLine($"Closest location (ranges): {tasks.Min(t => t.Result)}");
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
-
-        return Task.CompletedTask;
     }
 
     private void LoadMaps()
@@ -102,11 +134,8 @@ public class Day05 : IDayChallenge
     {
         //Look for the first range that our source value falls into
         var range = map.FirstOrDefault(m => m.SourceRangeStart <= sourceValue && m.SourceRangeEnd >= sourceValue);
+
         //None defined? Then the destination value is the same
-        if (range == null)
-            return sourceValue;
-        
-        //destination value will be the same distance from the start of its range as the source is from its
-        return range.DestinationRangeStart + (sourceValue - range.SourceRangeStart);
+        return range?.MapValue(sourceValue)??sourceValue;
     }
 }
